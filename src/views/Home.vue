5bb80f93e118c5ca72be50344247943cc343ccfe
BenjaminNewitt <benjaminnewitt@gmail.com>
2020-12-23 14:20:37 -0700

    setting up home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home"></div>
+  <div class="home container-fluid">
+    <div class="row">
+<!-- toolbar component -->
+    </div>
+    <div class="row">
+      <!-- give a little space -->
+      <div class="col-11 mx-auto">
+        <div class="row">
+        <!-- array of actorGroups components -->
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
