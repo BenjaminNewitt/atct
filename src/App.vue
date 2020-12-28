612236d77004ce0fb09f3604a92b726eda2e0abc
BenjaminNewitt <benjaminnewitt@gmail.com>
2020-12-28 10:17:02 -0700

    plan to move Toolbar to app out of router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <!-- <Toolbar /> -->
     <router-view/>
   </div>
 </template>
+
+<script>
+// import Toolbar from "@/components/Toolbar";
+export default {
+  name: "App"
+  // components: {
+  //   Toolbar
+  // }
+}
+</script>
 
 <style>
 
