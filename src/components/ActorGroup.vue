abd62052f82f127dfd24b56ed26e4668b3fd2f9d
BenjaminNewitt <benjaminnewitt@gmail.com>
2020-12-29 22:26:09 -0700

    updates note for actorGroup

--- src/components/ActorGroup.vue
+++ src/components/ActorGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="ActorGroup" class="col-3 border border-dark border-2">
-<!-- col-4 seems like a good size for now, will need to check overflow -->
+<!-- need to check overflow -->
   </div>
 </template>
 
